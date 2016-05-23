565808d0dbb430d9af2a423274497c4477cdc89b
Jim Howes <das.howes@gmail.com>
2016-05-23 22:25:31 +0000

    Static pages ahoy

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", "merchlord"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | merchlord"
+
+  end
+
+end
